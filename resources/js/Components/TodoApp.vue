5c299d2416bfaa77187b363a19e99dfcba36b8c9
mayk <mayk@gmail.com>
2024-10-28 10:05:21 +0800

    added an filter for groups

--- resources/js/Components/TodoApp.vue
+++ resources/js/Components/TodoApp.vue
@@ -3,14 +3,20 @@
         <h1 class="text-2xl font-bold my-4">Todo List</h1>
 
         <input v-model="searchQuery" class="border p-2 w-full my-4" placeholder="Search todos">
-
+        <div class="flex gap-1 my-1">
+            <select v-model="selectedFilterGroup" class="border p-1">
+                <option value="">All</option>
+                <option v-for="group in groups" :key="group.id" :value="group.id">{{ group.name }}</option>
+            </select>
+        </div>
+        <br>
         <div class="flex gap-2 my-2">
             <input v-model="newTodo" class="border p-2 w-full" placeholder="Add new todo" @keyup.enter="addTodo">
             <select v-model="selectedGroup" class="border p-2">
                 <option disabled value="">Select ...</option>
                 <option v-for="group in groups" :key="group.id" :value="group.id">{{ group.name }}</option>
             </select>
-            <button class="bg-blue-500 text-white p-2" @click="addTodo">Add</button>
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded" @click="addTodo">Add</button>
         </div>
 
         <div v-if="isEditing" class="my-4 border p-4 rounded">
@@ -20,8 +26,8 @@
                 <option disabled value="">Select group...</option>
                 <option v-for="group in groups" :key="group.id" :value="group.id">{{ group.name }}</option>
             </select>
-            <button class="bg-green-500 text-white p-2" @click="updateTodo">Update</button>
-            <button class="bg-gray-500 text-white p-2" @click="cancelEdit">Cancel</button>
+            <button class="bg-green-500 text-white p-2 mr-1 font-bold py-2 px-4 border border-blue-700 rounded" @click="updateTodo">Update</button>
+            <button class="bg-gray-500 text-white p-2 font-bold py-2 px-4 border border-blue-700 rounded" @click="cancelEdit">Cancel</button>
         </div>
 
         <ul>
@@ -29,8 +35,8 @@
                 <input type="checkbox" v-model="todo.completed" @change="updateTodoStatus(todo)">
                 <span :class="{ 'line-through': todo.completed }">{{ todo.title }}</span>
                 <span class="ml-2 text-gray-500">({{ todo.groupName }})</span>
-                <button class="ml-auto bg-yellow-500 text-white p-1" @click="editTodo(todo)">Edit</button>
-                <button class="ml-1 bg-red-500 text-white p-1" @click="deleteTodo(todo.id)">Delete</button>
+                <button class="ml-auto bg-yellow-500 text-white p-1 mb-2 font-bold py-2 px-4 border border-blue-700 rounded" @click="editTodo(todo)">Edit</button>
+                <button class="ml-1 bg-red-500 text-white p-1 mb-2 font-bold py-2 px-4 border border-blue-700 rounded" @click="deleteTodo(todo.id)">Delete</button>
             </li>
         </ul>
     </div>
@@ -45,6 +51,7 @@ export default {
             todos: [],
             newTodo: '',
             selectedGroup: null,
+            selectedFilterGroup: '', 
             groups: [],
             searchQuery: '',
             isEditing: false,
@@ -57,19 +64,16 @@ export default {
     },
     computed: {
         filteredTodos() {
-            return this.todos.filter(todo => 
-                todo.title.toLowerCase().includes(this.searchQuery.toLowerCase())
-            );
+            return this.todos.filter(todo => {
+                const matchesSearchQuery = todo.title.toLowerCase().includes(this.searchQuery.toLowerCase());
+                const matchesGroupFilter = !this.selectedFilterGroup || todo.group_id === this.selectedFilterGroup;
+                return matchesSearchQuery && matchesGroupFilter;
+            });
         }
     },
     methods: {
         async fetchTodos() {
-            const response = await axios.get('/api/todos', {
-                params: {
-                    group_id: this.selectedGroup  
-                }
-            });
-
+            const response = await axios.get('/api/todos');
             this.todos = response.data.map(todo => ({
                 ...todo,
                 groupName: this.groups.find(group => group.id === todo.group_id)?.name 
